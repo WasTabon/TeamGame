13600c9ae1eb083ec82708edd7f1ff31143d9db7
WasTabon <titanium.jen@gmail.com>
2025-07-23 01:35:40 +0300

    ui

--- Assets/TeamGame/Scripts/WalletController.cs
+++ Assets/TeamGame/Scripts/WalletController.cs
@@ -1,13 +1,90 @@
 using UnityEngine;
+using DG.Tweening;
+using System.Collections.Generic;
 
 public class WalletController : MonoBehaviour
 {
     public static WalletController Instance;
 
+    [SerializeField] private RectTransform _items;
+
     public int skillPoints;
+    public int money;
+
+    private const int ItemPrice = 500;
+    private HashSet<string> purchasedItems = new HashSet<string>();
 
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
+        LoadPurchasedItems();
+    }
+
+    public void BuyItem(RectTransform itemRect)
+    {
+        string itemId = itemRect.name;
+
+        if (purchasedItems.Contains(itemId))
+        {
+            Debug.Log($"Item '{itemId}' already purchased.");
+            return;
+        }
+
+        if (money < ItemPrice)
+        {
+            Debug.Log("Not enough money!");
+            return;
+        }
+
+        money -= ItemPrice;
+        purchasedItems.Add(itemId);
+        SavePurchasedItems();
+
+        AnimateAndRemove(itemRect);
+    }
+
+    private void AnimateAndRemove(RectTransform itemRect)
+    {
+        itemRect.DOScale(Vector3.zero, 0.4f)
+            .SetEase(Ease.InBack)
+            .OnComplete(() => itemRect.gameObject.SetActive(false));
+    }
+
+    private void SavePurchasedItems()
+    {
+        string savedData = string.Join(",", purchasedItems);
+        PlayerPrefs.SetString("PurchasedItems", savedData);
+        PlayerPrefs.SetInt("PlayerMoney", money);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadPurchasedItems()
+    {
+        purchasedItems.Clear();
+
+        string savedData = PlayerPrefs.GetString("PurchasedItems", "");
+        money = PlayerPrefs.GetInt("PlayerMoney", money);
+
+        if (!string.IsNullOrEmpty(savedData))
+        {
+            var items = savedData.Split(',');
+            foreach (var item in items)
+            {
+                purchasedItems.Add(item);
+            }
+        }
+        foreach (Transform child in _items) 
+        {
+            if (purchasedItems.Contains(child.name))
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
     }
 }
