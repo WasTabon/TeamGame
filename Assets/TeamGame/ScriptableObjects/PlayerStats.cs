74603935d47efc695cfbc9d15aac6945e26a28ad
WasTabon <titanium.jen@gmail.com>
2025-07-22 19:06:41 +0300

    ui

--- Assets/TeamGame/ScriptableObjects/PlayerStats.cs
+++ Assets/TeamGame/ScriptableObjects/PlayerStats.cs
@@ -1,4 +1,16 @@
+using System.IO;
 using UnityEngine;
+
+[System.Serializable]
+public class PlayerStatsData
+{
+    public string characterName;
+    public Sprite icon;
+    public int speed;
+    public int stamina;
+    public int attack;
+    public int defense;
+}
 
 [CreateAssetMenu(fileName = "NewPlayerStats", menuName = "Player/Stats", order = 1)]
 public class PlayerStats : ScriptableObject
@@ -9,4 +21,50 @@ public class PlayerStats : ScriptableObject
     public int stamina;
     public int attack;
     public int defense;
+    
+    private const string SaveFolder = "PlayerStatsSaves";
+
+    public void SaveToFile()
+    {
+        var data = new PlayerStatsData()
+        {
+            characterName = this.characterName,
+            speed = this.speed,
+            stamina = this.stamina,
+            attack = this.attack,
+            defense = this.defense
+        };
+
+        string json = JsonUtility.ToJson(data, true);
+
+        string dir = Path.Combine(Application.persistentDataPath, SaveFolder);
+        if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+
+        string path = Path.Combine(dir, $"{characterName}.json");
+        File.WriteAllText(path, json);
+        Debug.Log($"Сохранено в файл: {path}");
+    }
+    
+    public void LoadFromFile()
+    {
+        string path = Path.Combine(Application.persistentDataPath, SaveFolder, $"{characterName}.json");
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            PlayerStatsData data = JsonUtility.FromJson<PlayerStatsData>(json);
+
+            this.speed = data.speed;
+            this.stamina = data.stamina;
+            this.attack = data.attack;
+            this.defense = data.defense;
+
+            Debug.Log($"Загружено из файла: {path}");
+        }
+        else
+        {
+            Debug.LogWarning("Файл не найден: " + path);
+        }
+    }
 }
--- Assets/TeamGame/Scripts/PlayersController.cs
+++ Assets/TeamGame/Scripts/PlayersController.cs
@@ -1,9 +1,42 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayersController : MonoBehaviour
 {
+    [SerializeField] private GameObject _playerPanel;
+    
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI _playerNameText;
+    [SerializeField] private TextMeshProUGUI _playerSpeedText;
+    [SerializeField] private TextMeshProUGUI _playerStaminaText;
+    [SerializeField] private TextMeshProUGUI _playerAttackText;
+    [SerializeField] private TextMeshProUGUI _playerDefenseText;
+
+    [Header("Fill Bars")]
+    [SerializeField] private Image _playerSpeedFill;
+    [SerializeField] private Image _playerStaminaFill;
+    [SerializeField] private Image _playerAttackFill;
+    [SerializeField] private Image _playerDefenseFill;
+    
+    private PlayerStats _playerStats;
+    
     public void OpenPlayerPanel(PlayerStats playerStats)
     {
+        _playerStats = playerStats;
+
+        _playerPanel.SetActive(true);
         
+        _playerNameText.text = playerStats.characterName;
+        
+        _playerSpeedText.text = $"{playerStats.speed}/10";
+        _playerStaminaText.text = $"{playerStats.stamina}/10";
+        _playerAttackText.text = $"{playerStats.attack}/10";
+        _playerDefenseText.text = $"{playerStats.defense}/10";
+        
+        _playerSpeedFill.fillAmount = playerStats.speed / 10f;
+        _playerStaminaFill.fillAmount = playerStats.stamina / 10f;
+        _playerAttackFill.fillAmount = playerStats.attack / 10f;
+        _playerDefenseFill.fillAmount = playerStats.defense / 10f;
     }
 }
