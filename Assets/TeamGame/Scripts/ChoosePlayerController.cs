f5f292c92ad9b3452b9dd43f8f26a593d5c83a0e
WasTabon <titanium.jen@gmail.com>
2025-07-24 18:06:28 +0300

    ui

--- Assets/TeamGame/Scripts/ChoosePlayerController.cs
+++ Assets/TeamGame/Scripts/ChoosePlayerController.cs
@@ -14,16 +14,48 @@ public class ChoosePlayerController : MonoBehaviour
     [SerializeField] private Image _image2;
     [SerializeField] private Image _image3;
 
+    private bool _choose1;
+    private bool _choose2;
+    private bool _choose3;
+    
     public void ChoosePlayer1()
     {
-        
+        _choosePlayerPanel.SetActive(true);
+        _choose1 = true;
+        _choose2 = false;
+        _choose3 = false;
     }
     public void ChoosePlayer2()
     {
-        
+        _choosePlayerPanel.SetActive(true);
+        _choose2 = true;
+        _choose3 = false;
+        _choose1 = false;
     }
     public void ChoosePlayer3()
     {
-        
+        _choosePlayerPanel.SetActive(true);
+        _choose3 = true;
+        _choose2 = false;
+        _choose1 = false;
+    }
+
+    public void ChoosePlayer(PlayerStats playerStats)
+    {
+        if (_choose1)
+        {
+            _text1.text = playerStats.characterName;
+            _image1.sprite = playerStats.icon;
+        }
+        else if (_choose2)
+        {
+            _text2.text = playerStats.characterName;
+            _image2.sprite = playerStats.icon;
+        }
+        else if (_choose3)
+        {
+            _text3.text = playerStats.characterName;
+            _image3.sprite = playerStats.icon;
+        }
     }
 }
